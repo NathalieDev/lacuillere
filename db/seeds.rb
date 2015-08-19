1181237b3179cfe5f00db861026772f057902c7e
Nathalie Mesnard <nathalie.mesnard@gmail.com>
2015-08-19 15:55:31 +0200

    created seeds restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,57 @@ restaurants_attributes = [
   {
     name:         "Epicure au Bristol",
     address:      "112 rue du Fg St-Honoré 75008 Paris",
-    #description:  "Face au jardin, on découvre une salle lumineuse... et la cuisine d'Éric Frechon.",
     phone_number:  "01 45 65 75 32",
     category:      "french"
   },
   {
     name:         "La truffière",
     address:      "4 rue Blainville 75005 Paris",
-    #description:  "Une valeur sûre que cette belle maison du 17e et les recettes de Jean-Christophe Rizet",
     phone_number:  "01 45 65 40 66",
     category:      "italian"
   },
   {
     name:         "Le pré catelan",
     address:      "route de Suresnes 75016 Paris",
-    #description:  "Oeil vif, geste sûr: impossible de distinguer, dans les créations de Frédéric Anton...",
     phone_number:  "01 50 33 46 96",
     category:      "belgian"
+  },
+  {
+    name:         "Fraiche",
+    address:      "rue Colonel Fabien 75010 Paris",
+    phone_number:  "01 50 33 52 96",
+    category:      "french"
+  },
+  {
+    name:         "Clamato",
+    address:      "75011 Paris",
+    phone_number:  "01 40 33 46 90",
+    category:      "chinese"
   }
 ]
 restaurants_attributes.each { |params| Restaurant.create!(params) }
+top = Review.new(content: "c'était top", rating: 4)
+restaurant1 = Restaurant.find(1)
+top.restaurant = restaurant1
+top.save
+
+genial = Review.new(content: "accueil génial", rating: 3)
+restaurant2 = Restaurant.find(2)
+genial.restaurant = restaurant2
+genial.save
+
+sale = Review.new(content: "un rat courrait dans les cuisines", rating: 1)
+sale.restaurant = Restaurant.find(3)
+sale.save
+
+romantic = Review.new(content: "so romantic !", rating: 3)
+romantic.restaurant = Restaurant.find(4)
+romantic.save
+
+intox = Review.new(content: "je suis malade", rating:0)
+intox.restaurant = Restaurant.find(5)
+
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
